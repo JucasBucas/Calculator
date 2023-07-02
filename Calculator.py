0451192fc0f63f1c591ccd6274d50cdce1798458
JucasBucas <137675112+JucasBucas@users.noreply.github.com>
2023-07-02 13:12:42 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+intro = input("Hello! This is caltulator!You can add, substact, multiply and divide!Press Enter to continue")
+
+a = int(input("What number would the first number be? "))
+
+b = int(input("What number would the second number be? "))
+
+oper = input("What operation would you like to use? ")
+
+if oper == "+":
+    
+	print(a, "+", b, "=", a + b)
+
+elif oper == "-":
+    
+	print(a, "-", b, "=", a - b)
+
+elif oper == "*":
+    
+	print(a, "*", b, "=", a * b)
+
+elif oper == "/":
+    
+	print(a, "/", b, "=", a / b)
+
+else:
+   
+	print("Error")
